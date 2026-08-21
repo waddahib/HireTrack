b972e4074202062e5e77d1ba2ab7f987f7e57abd
Waddah Ibrahim <wibrahi3@uwo.ca>
2026-08-21 07:05:18 -0400

    Add job application API endpoints

--- main.py
+++ main.py
@@ -1,8 +1,35 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class JobApplication(BaseModel):
+    company: str
+    position: str
+    status: str
+    location: str | None = None
+    job_url: str | None = None
+    notes: str | None = None
+
+
+applications = []
 
 
 @app.get("/")
 def home():
     return {"message": "HireTrack API is running"}
+
+
+@app.post("/applications")
+def create_application(application: JobApplication):
+    applications.append(application)
+    return {
+        "message": "Application added successfully",
+        "application": application
+    }
+
+
+@app.get("/applications")
+def get_applications():
+    return applications
